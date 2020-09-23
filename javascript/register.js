4d2c5c1e08ac8002add8f0a25a53370ff73fec66
Maham Nazir <maham.nazir@vectorsolutions.com>
2020-09-24 02:06:25 +0500

    test

--- javascript/register.js
+++ javascript/register.js
@@ -17,7 +17,7 @@ function register(type) {
     else
     {
         // make checks here as well
-        $.post('nuces-backend-git-test-app.apps-crc.testing:8080/addUser', fields, function(data,status){
+        $.post('http://nuces-backend-git-test-app.apps-crc.testing:8080/addUser', fields, function(data,status){
             //if success move to home
             if (data[0] == 'success')
             {
