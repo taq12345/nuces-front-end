ee8c0f1dc3383edd54dc910d53a23186e181e159
Talha Qureshi <talha.ahmed@northbaysolutions.net>
2020-09-29 16:13:47 +0500

    test

---
+++ javascript/constants.js
@@ -0,0 +1,2 @@
+// const ROUTE = "http://nuces-backend-1-template-test.apps-crc.testing"
+const ROUTE = "http://localhost:3000"
--- javascript/register.js
+++ javascript/register.js
@@ -17,7 +17,7 @@ function register(type) {
     else
     {
         // make checks here as well
-        $.post('http://nuces-backend-git-test-app.apps-crc.testing/addUser', fields, function(data,status){
+        $.post('http://nuces-backend-1-template-test.apps-crc.testing/addUser', fields, function(data,status){
             //if success move to home
             if (data[0] == 'success')
             {
